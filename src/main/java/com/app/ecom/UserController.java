e84cab49b4a0bcab76325226f99d546aa8275ebc
devricardoperez <ricardoperezzeller@gmail.com>
2025-04-16 17:59:00 -0300

    jpa config

---
+++ src/main/java/com/app/ecom/UserController.java
@@ -0,0 +1,48 @@
+package com.app.ecom;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+//    @RequestMapping(value = "/api/users", method = RequestMethod.GET)
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.fectAllUsers(),
+                                    HttpStatus.OK);
+//        return ResponseEntity.ok(userService.fectAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        return userService.fetchUser(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody User user){
+        userService.addUser(user);
+        return ResponseEntity.ok("User created");
+    }
+
+    @PutMapping ("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id,
+                                             @RequestBody User updatedUser){
+        boolean update = userService.updateUser(updatedUser, id);
+        if(update){
+            return ResponseEntity.ok("User updated successfully");
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
---
+++ src/main/java/com/app/ecom/UserService.java
@@ -0,0 +1,39 @@
+package com.app.ecom;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private List<User> userList = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public List<User> fectAllUsers() {
+        return userList;
+    }
+
+    public void addUser(User user) {
+        user.setId(nextId++);
+        userList.add(user);
+    }
+
+    public Optional<User> fetchUser(Long id) {
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
+    }
+
+    public boolean updateUser(User updateUser, Long id) {
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .map(existingUser -> {
+                    existingUser.setFirstName(updateUser.getFirstName());
+                    existingUser.setLastName(updateUser.getLastName());
+                    return true;
+                }).orElse(false);
+    }
+}
