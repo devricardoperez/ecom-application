063c5f3cc12f196f9541e710572de38e2b47288f
devricardoperez <ricardoperezzeller@gmail.com>
2025-04-20 18:08:00 -0300

    Defining endPoint to Remove Item from Cart

--- src/main/java/com/app/ecom/controller/CartController.java
+++ src/main/java/com/app/ecom/controller/CartController.java
@@ -25,4 +25,14 @@ public class CartController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @DeleteMapping("/items/{productId}")
+    public ResponseEntity<Void> removeFromCart(
+            @RequestHeader("X-User-ID") String userdId,
+            @PathVariable Long productId){
+        boolean deleted = cartService.deleteItemFromCart(userdId, productId);
+        return deleted ? ResponseEntity.noContent().build()
+                       : ResponseEntity.notFound().build();
+
+    }
+
 }
