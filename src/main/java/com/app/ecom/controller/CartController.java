c215b86f28b1fded171c8807c9c2769486513e5e
devricardoperez <ricardoperezzeller@gmail.com>
2025-04-20 10:43:35 -0300

    Defining endpoint to Add user to Cart

---
+++ src/main/java/com/app/ecom/controller/CartController.java
@@ -0,0 +1,28 @@
+package com.app.ecom.controller;
+
+import com.app.ecom.dto.CartItemRequest;
+import com.app.ecom.model.CartItem;
+import com.app.ecom.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cart")
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping
+    public ResponseEntity<String> addToCart(
+            @RequestHeader("X-USER-ID") String userId,
+            @RequestBody CartItemRequest request) {
+        if (!cartService.addToCart(userId, request))
+             return ResponseEntity.badRequest().body("Product Out of Stock or User not Found or Product Not found");
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+}
